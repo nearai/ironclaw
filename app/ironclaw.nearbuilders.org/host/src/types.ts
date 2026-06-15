19d64037f37f3abc23ebc711051d960f82399468
Elliot Braem <elliot@ejlbraem.com>
2026-06-15 10:05:56 -0500

    Remove ironclaw.nearbuilders.org (should only use ironclaw.everything.dev)

--- app/ironclaw.nearbuilders.org/host/src/types.ts
+++
@@ -1,2 +0,0 @@
-export type { HeadData, RenderOptionsWithApi, RouterModule } from "everything-dev/ui/types";
-export type { RuntimeConfig } from "./services/config";
--- app/ironclaw.nearbuilders.org/ui/src/providers/index.tsx
+++
@@ -1,6 +0,0 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider } from "next-themes";
-import { Toaster } from "sonner";
-
-export * from "@/hooks";
-export { QueryClientProvider, ThemeProvider, Toaster };
