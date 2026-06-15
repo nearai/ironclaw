a8ae1b67be9f3f8cb7b2e8e61b78f03a17e3d95a
Elliot Braem <elliot@ejlbraem.com>
2026-06-15 11:33:04 -0500

    delete

--- app/ironclaw.nearbuilders.org/ui/src/routes/_layout/_authenticated/settings/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_layout/_authenticated/settings/")({
-  loader: async () => {
-    throw redirect({ to: "/settings/profile" });
-  },
-});
